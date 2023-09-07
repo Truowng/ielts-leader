37f6797294d48feeb78e0eac4434605e12acf9a9
Truowng <quockhanhtruong166@gmail.com>
2023-09-08 01:16:37 +0700

    homepage 90%

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,47 @@ export const setActive = (el) => {
     });
   });
 };
+
+const swiperCta = new Swiper(".cta .swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  speed: 750,
+  autoplay: {
+    delay: 2000,
+  },
+
+  breakpoints: {
+    1199: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiperBlog = new Swiper("#homepage-blog .swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  speed: 750,
+  autoplay: {
+    delay: 2000,
+  },
+
+  breakpoints: {
+    1199: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    767: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+});
